589216ee3576a49b8d1b59e89df809f4af79f651
astapleton <andrew@utvate.com>
2024-10-04 11:20:10 -0700

    i2c: Add I2C Controller implementation

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,7 +2,8 @@
 
 pub use crate::delay::DelayExt as _stm32h5xx_hal_delay_DelayExt;
 pub use crate::gpio::GpioExt as _stm32h5xx_hal_gpio_GpioExt;
-pub use crate::icache::ICacheExt as _stm32h5xx_hal_delay_ICacheExt;
+pub use crate::i2c::I2cExt as _stm32h5xx_hal_i2c_I2cExt;
+pub use crate::icache::ICacheExt as _stm32h5xx_hal_icache_ICacheExt;
 pub use crate::pwr::PwrExt as _stm32h5xx_hal_pwr_PwrExt;
 pub use crate::rcc::RccExt as _stm32h5xx_hal_rcc_RccExt;
 
