c26264d73b26ca40a03e8cee53f4593b02bfa797
astapleton <andrew@utvate.com>
2025-02-21 18:59:39 -0800

    Add I2C definitions for other H5 processors

--- src/i2c/i2c_def.rs
+++ src/i2c/i2c_def.rs
@@ -37,7 +37,7 @@ macro_rules! i2c {
 
                     match ccipr4.[<$I2CX:lower sel>]().variant() {
                         ccipr4::I2CSEL::Pclk => Some(clocks.$pclk()),
-                        ccipr4::I2CSEL::[< $pllX:camel R>] => clocks.[< $pllX:lower  _r_ck >](),
+                        ccipr4::I2CSEL::[< $pllX:camel R>] => clocks.[< $pllX:lower >]().r_ck(),
                         ccipr4::I2CSEL::HsiKer => clocks.hsi_ck(),
                         ccipr4::I2CSEL::CsiKer => clocks.csi_ck(),
                     }.expect("Source clock not enabled")
@@ -94,4 +94,120 @@ mod rm492 {
 
     i2c! { I2C1: pclk1, Pll2 }
     i2c! { I2C2: pclk1, Pll2 }
+}
+
+#[cfg(feature = "h523_h533")]
+mod h523_h533 {
+    use super::*;
+    use crate::stm32::{I2C1, I2C2, I2C3};
+
+    pins! {
+        I2C1:
+            SCL: [
+                gpio::PB6<Alternate<4, OpenDrain>>,
+                gpio::PB8<Alternate<4, OpenDrain>>
+            ]
+
+            SDA: [
+                gpio::PB7<Alternate<4, OpenDrain>>,
+                gpio::PB9<Alternate<4, OpenDrain>>
+            ]
+
+        I2C2:
+            SCL: [
+                gpio::PB10<Alternate<4, OpenDrain>>,
+                gpio::PF1<Alternate<4, OpenDrain>>
+            ]
+
+            SDA: [
+                gpio::PB3<Alternate<4, OpenDrain>>,
+                gpio::PB12<Alternate<4, OpenDrain>>,
+                gpio::PF0<Alternate<4, OpenDrain>>
+            ]
+
+        I2C3:
+            SCL: [
+                gpio::PA8<Alternate<4, OpenDrain>>,
+                gpio::PD6<Alternate<4, OpenDrain>>
+            ]
+
+            SDA: [
+                gpio::PB4<Alternate<9, OpenDrain>>,
+                gpio::PC9<Alternate<4, OpenDrain>>,
+                gpio::PD7<Alternate<4, OpenDrain>>
+            ]
+    }
+
+    i2c! { I2C1: pclk1, Pll3 }
+    i2c! { I2C2: pclk1, Pll3 }
+    i2c! { I2C3: pclk3, Pll3 }
+}
+
+#[cfg(feature = "h56x_h573")]
+mod h56x_h573 {
+    use super::*;
+    use crate::stm32::{I2C1, I2C2, I2C3, I2C4};
+
+    pins! {
+        I2C1:
+            SCL: [
+                gpio::PB6<Alternate<4, OpenDrain>>,
+                gpio::PB8<Alternate<4, OpenDrain>>
+            ]
+
+            SDA: [
+                gpio::PB7<Alternate<4, OpenDrain>>,
+                gpio::PB9<Alternate<4, OpenDrain>>
+            ]
+
+        I2C2:
+            SCL: [
+                gpio::PB10<Alternate<4, OpenDrain>>,
+                gpio::PF1<Alternate<4, OpenDrain>>,
+                gpio::PH4<Alternate<4, OpenDrain>>
+            ]
+
+            SDA: [
+                gpio::PB3<Alternate<4, OpenDrain>>,
+                gpio::PB11<Alternate<4, OpenDrain>>,
+                gpio::PB12<Alternate<4, OpenDrain>>,
+                gpio::PF0<Alternate<4, OpenDrain>>,
+                gpio::PH5<Alternate<4, OpenDrain>>
+            ]
+
+        I2C3:
+            SCL: [
+                gpio::PA8<Alternate<4, OpenDrain>>,
+                gpio::PH7<Alternate<4, OpenDrain>>
+            ]
+
+            SDA: [
+                gpio::PC9<Alternate<4, OpenDrain>>,
+                gpio::PH8<Alternate<4, OpenDrain>>
+            ]
+
+        I2C4:
+            SCL: [
+                gpio::PB6<Alternate<6, OpenDrain>>,
+                gpio::PB8<Alternate<6, OpenDrain>>,
+                gpio::PD12<Alternate<4, OpenDrain>>,
+                gpio::PF5<Alternate<4, OpenDrain>>,
+                gpio::PG7<Alternate<4, OpenDrain>>,
+                gpio::PH11<Alternate<4, OpenDrain>>
+            ]
+
+            SDA: [
+                gpio::PB7<Alternate<6, OpenDrain>>,
+                gpio::PB9<Alternate<6, OpenDrain>>,
+                gpio::PD13<Alternate<4, OpenDrain>>,
+                gpio::PF15<Alternate<4, OpenDrain>>,
+                gpio::PG6<Alternate<4, OpenDrain>>,
+                gpio::PH12<Alternate<4, OpenDrain>>
+            ]
+    }
+
+    i2c! { I2C1: pclk1, Pll3 }
+    i2c! { I2C2: pclk1, Pll3 }
+    i2c! { I2C3: pclk3, Pll3 }
+    i2c! { I2C4: pclk3, Pll3 }
 }
