c9d4336ad85000e75768a317e9b532945792eab3
Albin Hedman <albin9604@gmail.com>
2025-09-09 00:23:57 +0200

    NOT TESTED - impl eh-async::digital::Wait for awating pins

--- src/gpio/exti.rs
+++ src/gpio/exti.rs
@@ -1,5 +1,72 @@
+use core::{marker::PhantomData, ops::Deref};
+
 use super::{marker, Edge, Pin, PinExt};
-use crate::pac::{Interrupt, EXTI};
+use crate::{
+    gpio,
+    pac::{Interrupt, EXTI},
+};
+
+pub trait ExtiExt {
+    fn split(self) -> (Exti, ExtiChannels);
+}
+
+impl ExtiExt for EXTI {
+    fn split(self) -> (Exti, ExtiChannels) {
+        (
+            Exti(self),
+            ExtiChannels {
+                ch0: ExtiChannel,
+                ch1: ExtiChannel,
+                ch2: ExtiChannel,
+                ch3: ExtiChannel,
+                ch4: ExtiChannel,
+                ch5: ExtiChannel,
+                ch6: ExtiChannel,
+                ch7: ExtiChannel,
+                ch8: ExtiChannel,
+                ch9: ExtiChannel,
+                ch10: ExtiChannel,
+                ch11: ExtiChannel,
+                ch12: ExtiChannel,
+                ch13: ExtiChannel,
+                ch14: ExtiChannel,
+                ch15: ExtiChannel,
+            },
+        )
+    }
+}
+
+pub struct Exti(pub(crate) EXTI);
+
+impl Deref for Exti {
+    type Target = EXTI;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[non_exhaustive]
+pub struct ExtiChannel<const N: u8>;
+
+pub struct ExtiChannels {
+    pub ch0: ExtiChannel<0>,
+    pub ch1: ExtiChannel<1>,
+    pub ch2: ExtiChannel<2>,
+    pub ch3: ExtiChannel<3>,
+    pub ch4: ExtiChannel<4>,
+    pub ch5: ExtiChannel<5>,
+    pub ch6: ExtiChannel<6>,
+    pub ch7: ExtiChannel<7>,
+    pub ch8: ExtiChannel<8>,
+    pub ch9: ExtiChannel<9>,
+    pub ch10: ExtiChannel<10>,
+    pub ch11: ExtiChannel<11>,
+    pub ch12: ExtiChannel<12>,
+    pub ch13: ExtiChannel<13>,
+    pub ch14: ExtiChannel<14>,
+    pub ch15: ExtiChannel<15>,
+}
 
 impl<const P: char, const N: u8, MODE> Pin<P, N, MODE> {
     /// NVIC interrupt number of interrupt from this pin
@@ -30,25 +97,38 @@ impl<const P: char, const N: u8, MODE> Pin<P, N, MODE> {
 }
 
 /// External Interrupt Pin
-pub trait ExtiPin {
-    fn make_interrupt_source(&mut self, exti: &mut EXTI);
-    fn trigger_on_edge(&mut self, exti: &mut EXTI, level: Edge);
-    fn enable_event(&mut self, exti: &mut EXTI);
-    fn disable_event(&mut self, exti: &mut EXTI);
-    fn enable_interrupt(&mut self, exti: &mut EXTI);
-    fn disable_interrupt(&mut self, exti: &mut EXTI);
+pub trait ExtiPin<const P: char, const N: u8, M> {
+    fn make_interrupt_source(
+        self,
+        _ch: ExtiChannel<N>,
+        ch: &mut Exti,
+    ) -> Pin<P, N, M, true>;
+}
+
+// TODO: Find better name
+/// Only available on pins where interrupts have been enabled by the user
+pub trait ExtiedPin<const N: u8> {
+    fn trigger_on_edge(&mut self, exti: &mut Exti, level: Edge);
+    fn enable_event(&mut self, exti: &mut Exti);
+    fn disable_event(&mut self, exti: &mut Exti);
+    fn enable_interrupt(&mut self, exti: &mut Exti);
+    fn disable_interrupt(&mut self, exti: &mut Exti);
     fn clear_interrupt_pending_bit(&mut self, edge: Edge);
     fn check_interrupt(&self, edge: Edge) -> bool;
 }
 
-impl<PIN> ExtiPin for PIN
+impl<const P: char, const N: u8, M> ExtiPin<P, N, M>
+    for gpio::Pin<P, N, M, false>
 where
-    PIN: PinExt,
-    PIN::Mode: marker::Interruptable,
+    M: marker::Interruptable,
 {
     /// Make corresponding EXTI line sensitive to this pin
     #[inline(always)]
-    fn make_interrupt_source(&mut self, exti: &mut EXTI) {
+    fn make_interrupt_source(
+        self,
+        _ch: ExtiChannel<N>,
+        exti: &mut Exti,
+    ) -> Pin<P, N, M, true> {
         let i = self.pin_id();
         let port = self.port_id() as u32;
         let offset = 8 * (i % 4);
@@ -75,12 +155,16 @@ where
             }
             _ => unreachable!(),
         }
-    }
 
+        Pin { _mode: PhantomData }
+    }
+}
+
+impl<const P: char, const N: u8, M> ExtiedPin<N> for gpio::Pin<P, N, M, true> {
     /// Generate interrupt on rising edge, falling edge or both
     #[inline(always)]
-    fn trigger_on_edge(&mut self, exti: &mut EXTI, edge: Edge) {
-        let i = self.pin_id();
+    fn trigger_on_edge(&mut self, exti: &mut Exti, edge: Edge) {
+        let i = N;
         match edge {
             Edge::Rising => {
                 exti.rtsr1()
@@ -105,30 +189,30 @@ where
 
     /// Enable external interrupts from this pin.
     #[inline(always)]
-    fn enable_event(&mut self, exti: &mut EXTI) {
+    fn enable_event(&mut self, exti: &mut Exti) {
         exti.emr1()
-            .modify(|r, w| unsafe { w.bits(r.bits() | (1 << self.pin_id())) });
+            .modify(|r, w| unsafe { w.bits(r.bits() | (1 << N)) });
     }
 
     /// Disable external interrupts from this pin
     #[inline(always)]
-    fn disable_event(&mut self, exti: &mut EXTI) {
+    fn disable_event(&mut self, exti: &mut Exti) {
         exti.emr1()
-            .modify(|r, w| unsafe { w.bits(r.bits() & !(1 << self.pin_id())) });
+            .modify(|r, w| unsafe { w.bits(r.bits() & !(1 << N)) });
     }
 
     /// Enable external interrupts from this pin.
     #[inline(always)]
-    fn enable_interrupt(&mut self, exti: &mut EXTI) {
+    fn enable_interrupt(&mut self, exti: &mut Exti) {
         exti.imr1()
-            .modify(|r, w| unsafe { w.bits(r.bits() | (1 << self.pin_id())) });
+            .modify(|r, w| unsafe { w.bits(r.bits() | (1 << N)) });
     }
 
     /// Disable external interrupts from this pin
     #[inline(always)]
-    fn disable_interrupt(&mut self, exti: &mut EXTI) {
+    fn disable_interrupt(&mut self, exti: &mut Exti) {
         exti.imr1()
-            .modify(|r, w| unsafe { w.bits(r.bits() & !(1 << self.pin_id())) });
+            .modify(|r, w| unsafe { w.bits(r.bits() & !(1 << N)) });
     }
 
     /// Clear the interrupt pending bit for this pin
@@ -137,7 +221,7 @@ where
         unsafe {
             let exti = &(*EXTI::ptr());
 
-            let mask = 1 << self.pin_id();
+            let mask = 1 << N;
             match edge {
                 Edge::Rising => exti.rpr1().write(|w| w.bits(mask)),
                 Edge::Falling => exti.fpr1().write(|w| w.bits(mask)),
@@ -158,7 +242,7 @@ where
                 _ => panic!("Must choose a rising or falling edge"),
             };
 
-            bits & (1 << self.pin_id()) != 0
+            bits & (1 << N) != 0
         }
     }
 }
