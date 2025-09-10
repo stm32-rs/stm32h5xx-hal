7530eee5d7a40086018b8458bc3725ea16de96bf
astapleton <andrew@farcast.com>
2025-09-10 10:27:40 -0700

    Add extension trait to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -9,6 +9,7 @@ pub use crate::icache::ICacheExt as _stm32h5xx_hal_icache_ICacheExt;
 pub use crate::pwr::PwrExt as _stm32h5xx_hal_pwr_PwrExt;
 pub use crate::rcc::RccExt as _stm32h5xx_hal_rcc_RccExt;
 pub use crate::spi::SpiExt as _stm32h5xx_hal_spi_SpiExt;
+pub use crate::serial::usart::SerialExt as _stm32h5xx_hal_serial_SerialExt;
 pub use crate::usb::UsbExt as _stm32h5xx_hal_usb_UsbExt;
 
 pub use crate::time::U32Ext as _;
